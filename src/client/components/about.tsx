a9684baa5507a8ea5bd32657faeb927cd23bf12f
Jeremaih-Hill <JHillInquiries@gmail.com>
2021-04-13 12:14:22 -0500

    Updated About Page

--- src/client/components/about.tsx
+++ src/client/components/about.tsx
@@ -13,7 +13,7 @@ const About = (props: AboutProps) => {
                             <br />
                             <h3 className="row-md-6 content centered" >    That is the goal we strive to achieve with the creation of Vitality.</h3>
                             <br />
-                            <h3 className="row-md-6 content centered">  Cardio vascular disease,more commonly known as Heart disease, is one of the most pressing health tragedies in modern human life. An average of 647,000 Americans die from heart disease(1 out of every 4 US deaths)
+                            <h3 className="row-md-6 content centered">  Cardio vascular disease, more commonly known as Heart disease, is one of the most pressing health tragedies in modern human life. An average of 647,000 Americans die from heart disease(1 out of every 4 US deaths)
                                 on a yearly basis, making it the leading cause of death in the United States.</h3>
                             <br />
                             <br />
@@ -57,7 +57,206 @@ const About = (props: AboutProps) => {
                         Healthier alternatives for Food and Drinks, Managing a Healthy Weight, Getting Regular Physical Activity, and Avoiding habits that may lead to greater risks such as Smoking</h4>
                             <br />
 
+                            <section id="about">
 
+        <div className="inner wrapper posts-container fadeInLeft animated wow">
+
+            <div className="container">
+
+                <h2>What is Vitality?</h2>
+
+                <p> Our mission at Vitality is to create a world free of heart disease. According to the Top 10 Leading
+                    Causes of Death in the United States, Heart Disease is the #1 cause of death followed by Cancer,
+                    Unintentional Injuries, Respiratory Diseases, Stroke, Alzheimer's Disease, Diabetes, etc. Vitality
+                    is an App developed to address one of the world most pressing issue's: heart disease. Vitality builds stronger and informed communities by providing an interface were users can register
+                    for upcoming group fitness activities, view interactive videos and locate healthier food choices.
+                </p>
+
+                <div className="inner section">
+                    <div className="row">
+                        <div className="col-lg-12 about-us">
+                            <div className="row">
+                                <div className="col-md-6">
+
+                                    <h3>What Is Heart Disease?</h3>
+
+                                    <p>
+                                        The term "<b>heart disease</b>" refers to several types of heart conditions. The most
+                                        common type of heart disease in the United States is <b>coronary artery disease </b> 
+                                        (<b>CAD</b>), which affects the blood flow to the heart. Decreased blood flow can cause
+                                        a heart attack.
+                                    </p>
+
+                                    <h3>What Are The Symptoms?</h3>
+
+                                    <p>
+                                        Sometimes heart disease may be "silent" and not diagnosed until a person
+                                        experiences signs or symptoms of a <b>heart attack</b>, <b>heart failure</b>, or an 
+                                        <b> arrhythmia</b>. When these events happen, symptoms may include:
+                                    </p>
+
+                                        <li>
+                                        <b>Heart Attack</b>: Chest pain or discomfort, upper back or neck pain,
+                                        indigestion, heartburn, nausea or vomiting, extreme fatigue, upper body
+                                        discomfort, dizziness, and shortness of breath.
+                                        </li>
+
+                                        <li>
+                                        <b>Arrhythmia</b>: Fluttering feeling in the chest (palpitations).
+                                        </li>
+
+                                        <li>
+                                        <b>Heart Failure</b>: Shortness of breath, fatigue, or swelling of the feet,
+                                        ankles, legs, abdomen, or neck veins.
+                                        </li>
+
+                                    <h3>What Are The Risk Factors for Heart Disease?</h3>
+
+                                    <p>
+                                        <b>High blood pressure</b>, <b>high blood cholesterol</b> and <b>smoking</b> are key risk factors
+                                        for heart disease. About half of Americans (47%) have at least one of these
+                                        three risk factors. Several other medical conditions and lifestyle choices can
+                                        also put people at higher risk for heart disease, including:
+                                    </p>
+                                    
+                                        <li>Diabetes</li>
+
+                                        <li>Overweight and Obesity</li>
+
+                                        <li>Unhealthy Diet</li>
+
+                                        <li>Physical Inactivity</li>
+
+                                        <li>Excessive Alcohol Use</li>
+                        
+                                    <h3>How Does it Affect Men?</h3>
+
+                                        <li>
+                                        Heart Disease is the leading cause of death for men in the United
+                                        States, killing 357,761 men in 2019--that's about 1 in every 4 male deaths.
+                                        </li>
+
+                                        <li>
+                                        <b>Heart disease</b> is the leading cause of death for men of most racial and
+                                        ethnic groups in the United States, including African Americans, American
+                                        Indians or Alaska Natives, Hispanics, and whites. For Asian American or
+                                        Pacific Islander men, heart disease is second only to cancer.
+                                        </li>
+
+                                        <li>
+                                        About 1 in 13 (7.7%) white men and 1 in 14 (7.1%) black men have coronary
+                                        heart disease. About 1 in 17 (5.9%) Hispanic men have coronary heart
+                                        disease.
+                                        </li>
+
+                                        <li>
+                                        Half of the men who die suddenly of coronary heart disease had no previous
+                                        symptoms. Even if you have no symptoms, you may still be at risk for heart
+                                        disease.
+                                        </li>
+                                    
+                                    <h3>How Does it Affect Women?</h3>
+                        
+                                        <li>
+                                        Heart disease is the leading cause of death for women in the United States,
+                                        killing 299,578 women in 2017—or about 1 in every 5 female deaths.
+                                        </li>
+
+                                    <li>
+                                        Heart disease is the leading cause of death for African American and white
+                                        women in the United States. Among American Indian and Alaska Native women,
+                                        heart disease and cancer cause roughly the same number of deaths each year.
+                                        For Hispanic and Asian or Pacific Islander women, heart disease is second
+                                        only to cancer as a cause of death.
+                                        </li>
+
+                                    <li>
+                                        About 1 in 16 women age 20 and older (6.2%) have coronary heart disease, the
+                                        most common type of heart disease.
+                                        </li>
+
+                                    <li>
+                                        About 1 in 16 white women (6.1%), black women (6.5%), and Hispanic women
+                                        (6%)
+                                        </li>
+
+                                    <li>
+                                        About 1 in 30 Asian women (3.2%)
+                                        </li>
+                                    
+                                    <h3>What Can You Do to Lower Your Risk of Heart Disease?</h3>
+
+                                        <li>
+                                        Know your blood pressure. Having uncontrolled blood pressure can result in
+                                        heart disease. High blood pressure has no symptoms so it's important to have
+                                        your blood pressure checked regularly.
+                                        </li>
+
+                                    <li>
+                                        Talk to your health care provider about whether you should be tested for
+                                        diabetes. Having diabetes raises your risk of heart disease.
+                                        </li>
+
+                                    <li>
+                                        Quit smoking. If you don't smoke, don't start. If you do smoke, learn ways
+                                        to quit.
+                                        </li>
+
+                                    <li>
+                                        Discuss checking your cholesterol and triglyceride levels with your health
+                                        care provider.
+                                        </li>
+
+                                    <li>
+                                        Make healthy food. Having overweight or obesity raises your risk of heart
+                                        disease.
+                                        </li>
+
+                                    <li>
+                                        Limit alcohol intake to one drink a day.
+                                        </li>
+
+                                    <li>
+                                        Lower your stress level and find healthy ways to cope with stress.
+                                        </li>
+
+                                    <h3>What Does Vitality Provide?</h3>
+
+                                    <p>
+                                        Vitality provides our users information about:
+                                    </p>
+
+                                        <li>
+                                            What is Heart Disease and how to prevent it.
+                                        </li>
+
+                                        <li>
+                                            A health module to determine how at risk the user is of
+                                            getting heart disease based on their health profile.
+                                        </li>
+
+                                        <li>
+                                            Educational and motivational videos that talk about healthy
+                                            habits, exercises, recipes and nutrition (Cardiac Rehabilitation).
+                                        </li>
+
+                                        <li>
+                                            Hosting events to promote others to do more healthy
+                                            activities.
+                                        </li>
+
+                                        <li>
+                                            Look at the latest news and see what's going on with the
+                                            website.
+                                        </li>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
                         </div>
                     </div>
